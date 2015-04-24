5df6ba37d2549e3d006d37a009800fa052791ea4
Ethan Pailes <ethanpailes@gmail.com>
2015-04-23 21:43:19 -0400

    got info extractor working

--- universities/extract_info.py
+++ universities/extract_info.py
@@ -7,8 +7,8 @@ Description: A script to pull coordinates from settlement data
 import re
 import json
 
-# IN_FILE = "university_articles.txt"
-IN_FILE = "mini.txt"
+IN_FILE = "uni_data.txt"
+#IN_FILE = "mini.txt"
 OUT_FILE = "uni_info.txt"
 
 PAGES_TO_STORE = 10000
@@ -19,32 +19,44 @@ WIKI_MARKUP_BLOCK_REGEX = re.compile("{{(.*)}}")
 
 INFOBOX_BEGIN_REGEX = re.compile("{{ *Infobox *[Uu]niversity")
 
-COORD_KEYS = [ "latd", "latm", "latNS", "longd", "longm", "longEW"] #TODO delete
 EST_KEYS = ["established"]
 COORDINATE_KEY = "coor"
+
+YEAR_REGEX = re.compile("([0-9]{3,4})")
+
 
 COORDINATE_PARSING_SCHEMA = [
     (re.compile(
     "[Cc]oord\|[0-9]+\|[0-9]+\|[0-9]+\|[NS]\|[0-9]+\|[0-9]+\|[0-9]+\|[EW]\|"),
         [
             (None, lambda t, s: None),
-            ("lat", lambda t, s: float(s)),
-            ("lat", lambda t, s: t + (float(s) * (1.0/60.0))),
-            ("lat", lambda t, s: t + (float(s) * (1.0/3600.0))),
-            ("lat", lambda t, s: t if s == "N" else -t),
+            ("lat", lambda t, s: float(s.strip())),
+            ("lat", lambda t, s: t + (float(s.strip()) * (1.0/60.0))),
+            ("lat", lambda t, s: t + (float(s.strip()) * (1.0/3600.0))),
+            ("lat", lambda t, s: t if s.strip() == "N" else -t),
 
-            ("long", lambda t, s: float(s)),
-            ("long", lambda t, s: t + (float(s) * (1.0/60.0))),
-            ("long", lambda t, s: t + (float(s) * (1.0/3600.0))),
-            ("long", lambda t, s: t if s == "N" else -t)
+            ("long", lambda t, s: float(s.strip())),
+            ("long", lambda t, s: t + (float(s.strip()) * (1.0/60.0))),
+            ("long", lambda t, s: t + (float(s.strip()) * (1.0/3600.0))),
+            ("long", lambda t, s: t if s.strip() == "E" else -t)
         ]),
-    # TODO add the double-number form here
+    (re.compile("[Cc]oord\|[0-9]+\|[0-9.-]+\|[NS]\|[0-9]+\|[0-9.-]+\|[EW]\|"),
+        [
+            (None, lambda t, s: None),
+            ("lat", lambda t, s: float(s.strip())),
+            ("lat", lambda t, s: t + (float(s.strip()) * (1.0/60.0))),
+            ("lat", lambda t, s: t if s.strip() == "N" else -t),
+
+            ("long", lambda t, s: float(s.strip())),
+            ("long", lambda t, s: t + (float(s.strip()) * (1.0/60.0))),
+            ("long", lambda t, s: t if s.strip() == "E" else -t)
+        ]),
     (re.compile("[Cc]oord\|[0-9.-]+\|[0-9.-]+\|"),
         [
             (None, lambda t, s: None),
-            ("lat", lambda t, s: float(s)),
+            ("lat", lambda t, s: float(s.strip())),
 
-            ("long", lambda t, s: float(s))
+            ("long", lambda t, s: float(s.strip()))
         ]),
 ]
 
@@ -140,16 +152,18 @@ class University(object):
         # locate date est
         for key in EST_KEYS:
             if key in d:
-                self.est = d[key]
+                try:
+                    self.est = YEAR_REGEX.search(d[key]).group(1)
+                except AttributeError:
+                    pass
                 return
 
     def write_to_file(self, outfilehandle):
         if (self.coords != None
                     and self.coords.coord_str != None
                     and self.est != None):
-            data_str = "{}\t{}".format(self.est, self.coords.coord_str)
-            print(data_str)
-            #outfilehandle.write(self.coords.coord_str)
+            data_str = "{}\t{}\n".format(self.est, self.coords.coord_str)
+            outfilehandle.write(data_str)
 
     def hit_eof(self):
         return self.eof
