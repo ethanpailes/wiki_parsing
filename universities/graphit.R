f98d89659975802d4b5bd71ca5219e7b4e7c9d76
Ethan Pailes <ethanpailes@gmail.com>
2015-04-26 21:52:38 -0400

    added output

--- universities/graphit.R
+++ universities/graphit.R
@@ -9,9 +9,8 @@ library(data.table)
 
 ###############################################################################
 WIKI_HOME = "~/Documents/tufts/text_mining/wiki/"
-SETTLEMENT_HOME = paste0(WIKI_HOME, "settlements/")
-DATA_DIR = paste0(WIKI_HOME, "data/")
-OUT_DIR = paste0(SETTLEMENT_HOME, "output/")
+UNIVERCITY_HOME = paste0(WIKI_HOME, "universities/")
+OUT_DIR = paste0(UNIVERCITY_HOME, "output/")
 
 XLIM = c(-150, 150)
 YLIM = c(-70, 70)
@@ -28,10 +27,10 @@ land = "grey"
 water = "grey80"
 bgColor = "grey80"
 
-locs = read.csv(paste0(SETTLEMENT_HOME, "coordinates.txt"),
+locs = read.csv(paste0(UNIVERCITY_HOME, "uni_info.txt"),
                    stringsAsFactors = F, header = F, sep = "\t")
 
-humanLocations = geom_point(data = locs, color = "red", alpha = .75, size = 1, aes(y = V1, x = V2))
+humanLocations = geom_point(data = locs, color = "red", alpha = .75, size = 1, aes(y = V2, x = V3))
 
 p = ggplot() + coord_map(xlim = XLIM, ylim = YLIM) +
     geom_polygon(data=worldmap, aes(X,Y,group=PID), size = 0.1, colour=land, fill=water, alpha=1, ) +
@@ -39,5 +38,5 @@ p = ggplot() + coord_map(xlim = XLIM, ylim = YLIM) +
   
 print(p)
   
-ggsave(file = paste0(OUT_DIR, "wiki_settlements.png"),
+ggsave(file = paste0(OUT_DIR, "wiki_universities.png"),
          plot = p, dpi = 600, width = 10, height = 6)
